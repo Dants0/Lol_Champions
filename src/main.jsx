26a0f39ab2538cc2988ee8734228a0581b359f81
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2024-04-05 16:06:09 -0300

    feat: add analyse

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Toaster position="top-right" />
+    <Analytics/>
     <App />
   </React.StrictMode>
 )
